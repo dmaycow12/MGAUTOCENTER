4c9ac05e0ff767d0e9bd39f2b1e436f459b1ddab
Maycow Douglas <dmaycow12@gmail.com>
2026-02-24 03:09:44 +0000

    File changes

--- src/pages/OrdemServico.jsx
+++ src/pages/OrdemServico.jsx
@@ -147,7 +147,7 @@ export default function OrdemServico() {
           </button>
         </div>
       ) : (
-        <div className="space-y-3">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {filtradas.map(os => (
             <OSCard
               key={os.id}
