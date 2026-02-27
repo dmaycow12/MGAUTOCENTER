7fbbc22edbc0643d8b735f0cce700fb9194626bb
Maycow Douglas <dmaycow12@gmail.com>
2026-02-27 22:21:41 +0000

    File changes

--- src/pages/NotasFiscais.jsx
+++ src/pages/NotasFiscais.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { base44 } from "@/api/base44Client";
 import {
   FileText, Plus, Upload, Search, Trash2, Eye, X,
-  CheckCircle, AlertCircle, Printer, Download, PlusCircle, MinusCircle, RefreshCw
+  CheckCircle, AlertCircle, Printer, Download, PlusCircle, MinusCircle, RefreshCw, ChevronDown
 } from "lucide-react";
 import ModalEntradaNF from "@/components/notas/ModalEntradaNF";
 
