7e2c3da0424c67795be74893371e007530ba36db
Maycow Douglas <dmaycow12@gmail.com>
2026-02-27 20:39:05 +0000

    File changes

--- src/pages/OrdemServico.jsx
+++ src/pages/OrdemServico.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { base44 } from "@/api/base44Client";
 import { Plus, Search, Edit, Trash2, MessageCircle, Printer, X, ChevronDown } from "lucide-react";
 import OSForm from "@/components/os/OSForm";
